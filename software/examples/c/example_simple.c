801389a26be6d9d6021741f3b39cb7b18700e5f0
Matthias Bolte <matthias@tinkerforge.com>
2016-05-18 10:44:56 +0200

    c: Add missing device destroy call

--- software/examples/c/example_simple.c
+++ software/examples/c/example_simple.c
@@ -34,6 +34,7 @@ int main(void) {
 
 	printf("Press key to exit\n");
 	getchar();
+	industrial_dual_analog_in_destroy(&idai);
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
 	return 0;
 }
