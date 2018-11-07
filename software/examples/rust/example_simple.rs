d0481375f6cbe1157ec875fe96bd6be47dde0b7a
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:22:30 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,21 +1,21 @@
 use std::{error::Error, io};
 
-use tinkerforge::{industrial_dual_analog_in_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_dual_analog_in_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual Analog In Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual Analog In Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_dual_analog_in_bricklet = IndustrialDualAnalogInBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let idai = IndustrialDualAnalogInBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current voltage from channel 1
-    let voltage = industrial_dual_analog_in_bricklet.get_voltage(1).recv()?;
-    println!("Voltage (Channel 1): {}{}", voltage as f32 / 1000.0, " V");
+    // Get current voltage from channel 1.
+    let voltage = idai.get_voltage(1).recv()?;
+    println!("Voltage (Channel 1): {} V", voltage as f32 / 1000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
