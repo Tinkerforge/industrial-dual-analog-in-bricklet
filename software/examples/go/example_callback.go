1c342d5a3843272038f5c5492350f736a0bd6aa0
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:10:42 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/industrial_dual_analog_in_bricklet"
+	"tinkerforge/ipconnection"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Industrial Dual Analog In Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	idai, _ := industrial_dual_analog_in_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	idai.RegisterVoltageCallback(func(channel uint8, voltage int32) {
+		fmt.Printf("Channel: %d\n", channel)
+		fmt.Printf("Voltage: %d V\n", float64(voltage)/1000.0)
+		fmt.Println()
+	})
+
+	// Set period for voltage (channel 1) receiver to 1s (1000ms).
+	// Note: The voltage (channel 1) callback is only called every second
+	//       if the voltage (channel 1) has changed since the last call!
+	idai.SetVoltageCallbackPeriod(1, 1000)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
---
+++ software/examples/go/example_threshold.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/industrial_dual_analog_in_bricklet"
+	"tinkerforge/ipconnection"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Industrial Dual Analog In Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	idai, _ := industrial_dual_analog_in_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Get threshold receivers with a debounce time of 10 seconds (10000ms).
+	idai.SetDebouncePeriod(10000)
+
+	idai.RegisterVoltageReachedCallback(func(channel uint8, voltage int32) {
+		fmt.Printf("Channel: %d\n", channel)
+		fmt.Printf("Voltage: %d V\n", float64(voltage)/1000.0)
+		fmt.Println()
+	})
+
+	// Configure threshold for voltage (channel 1) "greater than 10 V".
+	idai.SetVoltageCallbackThreshold(1, '>', 10*1000, 0)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
