87bed5f4644141d7043527f14338d3cb29c05e0c
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:54:39 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  industrial_dual_analog_in_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{industrial_dual_analog_in_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,25 +10,25 @@ fn main() -> Result<(), Box<dyn Error>> {
     let idai = IndustrialDualAnalogInBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let voltage_receiver = idai.get_voltage_callback_receiver();
+    let voltage_receiver = idai.get_voltage_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `idai` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for voltage in voltage_receiver {           
-                		println!("Channel: {}", voltage.channel);
-		println!("Voltage: {} V", voltage.voltage as f32 /1000.0);
-		println!();
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `idai` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for voltage in voltage_receiver {
+            println!("Channel: {}", voltage.channel);
+            println!("Voltage: {} V", voltage.voltage as f32 / 1000.0);
+            println!();
+        }
+    });
 
-		// Set period for voltage (channel 1) receiver to 1s (1000ms).
-		// Note: The voltage (channel 1) callback is only called every second
-		//       if the voltage (channel 1) has changed since the last call!
-		idai.set_voltage_callback_period(1, 1000);
+    // Set period for voltage (channel 1) receiver to 1s (1000ms).
+    // Note: The voltage (channel 1) callback is only called every second
+    //       if the voltage (channel 1) has changed since the last call!
+    idai.set_voltage_callback_period(1, 1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
